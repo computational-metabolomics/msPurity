364137edb248463124379f0e59c2673c5a2aea12
Thomas Lawson <thomas.nigel.lawson@gmail.com>
2018-06-13 15:46:20 +0100

    unit test updates

--- tests/testthat/test.flagremove.R
+++ tests/testthat/test.flagremove.R
@@ -5,12 +5,24 @@ test_that("checking flag and remove peaks", {
   library(msPurity)
   library(xcms)
 
-  msPths <- list.files(system.file("extdata", "lcms", "mzML", package="msPurityData"), full.names = TRUE)
+  msPths <- dirname(list.files(system.file("extdata", "lcms", "mzML", package="msPurityData"), full.names = TRUE))
+
+  msPths[1] <- file.path(msPths[1], 'LCMS_1.mzML')
+  msPths[2] <- file.path(msPths[2], 'LCMS_2.mzML')
+  msPths[3] <- file.path(msPths[3], 'LCMSMS_1.mzML')
+  msPths[4] <- file.path(msPths[4], 'LCMSMS_2.mzML')
+
   xset <- xcms::xcmsSet(msPths)
+
   xset@phenoData[,1] <- c('blank', 'blank', 'sample', 'sample')
+
+  print(xset@phenoData)
   xset <- xcms::group(xset)
   fr = msPurity::flag_remove(xset)
-  # expect_equal(nrow(fr$grp_peaklist), 14) # passing on testthat but fialing on check (not sure why)
+
+  print(head(fr$grp_peaklist))
+  expect_equal(nrow(fr$grp_peaklist), 14) # passing on testthat but failng on check (not sure why)
+
   # testthat::expect_equal(nrow(fr[[2]]), 14)
 
 
