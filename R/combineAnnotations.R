2d9880727f061026c12de795a5adee9a9361fcb1
Thomas N Lawson <thomas.nigel.lawson@gmail.com>
2019-09-05 16:58:10 +0100

    Combine ann fix (#61)
    * further doc updates
    
    * combineAnn fix
    
    * adduct reporting fix for combineAnnotations

--- R/combineAnnotations.R
+++ R/combineAnnotations.R
@@ -27,7 +27,7 @@
 #' @param compoundDbHost character; Database host (only applicable for postgres and mysql)
 #' @param compoundDbPort character; Database port (only applicable for postgres and mysql)
 #' @param compoundDbUser character; Database user (only applicable for postgres and mysql)
-#' @param compoundDbPass character; Database pass - Note this is not secure! use with caution (only applicable for postgres and mysql)
+#' @param compoundDbPass character; Database pass (only applicable for postgres and mysql) - Note this is not secure!
 #'
 #'
 #'
@@ -132,17 +132,16 @@ combineAnnotations <- function(sm_resultPth,
 
   # Add anything that is not found (e.g. will be a few cases where pubchem won't have inchikey but the spectral matching result will have)
   missing_comps <- metab_compounds[!metab_compounds$inchikey %in% metab_compounds_m$inchikey,]
-
   missing_comps <- cbind(missing_comps, splitInchis(missing_comps$inchikey))
   colnames(missing_comps)[colnames(missing_comps)=='pubchem_id'] = 'pubchem_cids'
-  # Remove rows we won't be using from missing_comps
+
   keep_cols = c('inchikey', 'inchikey1', 'inchikey2', 'inchikey3', 'pubchem_cids',
                 'exact_mass', 'molecular_formula', 'name')
   missing_comps <- missing_comps[,keep_cols]
   findx <- sapply(missing_comps, is.factor)
   missing_comps[findx] <- lapply(missing_comps[findx], as.character)
+  missing_comps$exact_mass <- as.numeric(missing_comps$exact_mass)
   metab_compounds_m <- dplyr::bind_rows(metab_compounds_m, missing_comps)
-
 
   # remove the old table and add the modified table
   DBI::dbExecute(con, 'DROP TABLE metab_compound')
@@ -249,7 +248,12 @@ getBoundedSiriusScore <- function(x, rs){
   if(nrow(scores)==1){
     bounded_score <- 1
   }else{
-    bounded_score <- negMinMax(abs(scores$Score))
+    if ('Score' %in% colnames(scores)){
+      score <- abs(scores$Score)
+    }else{
+      score <- abs(scores$score)
+    }
+    bounded_score <- negMinMax(score)
   }
   scores$bounded_score <- bounded_score
   return(scores)
@@ -513,7 +517,8 @@ combineScoresGrp <- function(c_peak_group, weights, con){
   if(DBI::dbExistsTable(con, 'sirius_csifingerid_results')){
     # check if adduct column present
     sirius_cols <- DBI::dbGetQuery(con, 'PRAGMA table_info(sirius_csifingerid_results)')
-    if ('adduct' %in% sirius_cols){
+
+    if ('adduct' %in% sirius_cols$name){
       adduct_colstr <- 's.adduct AS sirius_adduct'
     }else{
       adduct_colstr <- "'' AS sirius_adduct"
@@ -542,7 +547,7 @@ combineScoresGrp <- function(c_peak_group, weights, con){
 
   if(DBI::dbExistsTable(con, 'metfrag_results')){
     metfrag_cols <- DBI::dbGetQuery(con, 'PRAGMA table_info(metfrag_results)')
-    if ('adduct' %in% metfrag_cols){
+    if ('adduct' %in% metfrag_cols$name){
       adduct_colstr <- 'm.adduct AS metfrag_adduct'
     }else{
       adduct_colstr <- "'' AS metfrag_adduct"
@@ -651,6 +656,8 @@ combineScoresGrp <- function(c_peak_group, weights, con){
 
   # Order
   combined_df <- combined_df[order(combined_df$wscore, decreasing=TRUE),]
+
+
 
   # in most cases the adduct will be the same but there is a chance a different naming was used
   # or that there are adduct types that have give the same neutral mass
