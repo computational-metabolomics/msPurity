5b3b1216166e82c1f0d1085d65a8ddd27aded9db
tomnl <thomas.nigel.lawson@gmail.com>
2021-01-08 12:14:43 +0000

    add copyright notice to files

--- R/flag-filter-remove.R
+++ R/flag-filter-remove.R
@@ -1,3 +1,22 @@
+# msPurity R package for processing MS/MS data - Copyright (C)
+#
+# This file is part of msPurity.
+#
+# msPurity is a free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# msPurity is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with msPurity.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 #' @title Flag and remove unwanted peaks
 #'
 #' @description
--- R/spectralMatching.R
+++ R/spectralMatching.R
@@ -1,3 +1,22 @@
+# msPurity R package for processing MS/MS data - Copyright (C)
+#
+# This file is part of msPurity.
+#
+# msPurity is a free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# msPurity is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with msPurity.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 #' @title Spectral matching for LC-MS/MS datasets
 #' @aliases spectralMatching
 #' @description
@@ -948,9 +967,9 @@ alignAndMatch <- function(q_speaksi, l_speaksi, q_ppmProd, l_ppmProd, raW, mzW){
   # Match
   ##################
   dpcOut <- dpc(aligned$q, aligned$l)
-  
-  rl <- aligned$l[!aligned$l==0] 
-  rq <- aligned$q[!aligned$l==0] 
+
+  rl <- aligned$l[!aligned$l==0]
+  rq <- aligned$q[!aligned$l==0]
   rdpcOut <- dpc(rq, rl)
 
   cdpcOut <- cdpc(aligned$q, aligned$l)
