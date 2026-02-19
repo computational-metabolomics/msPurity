99033055582c53d4dbd7f3fcb70376827956bc9f
Thomas N Lawson <thomas.nigel.lawson@gmail.com>
2026-02-19 15:26:18 +0000

    Use zenodo reference to library spectra

--- R/spectralMatching.R
+++ R/spectralMatching.R
@@ -288,7 +288,25 @@ spectralMatching <- function(
 
 
   if (is.na(l_dbPth)){
-    l_dbPth <- system.file("extdata", "library_spectra", "library_spectra.db", package="msPurityData")
+
+    bfc <- BiocFileCache::BiocFileCache(ask = FALSE)
+    cache_name <- "msPurity_library_spectra_db"
+    cache_url <- "https://zenodo.org/records/18700802/files/library_spectra.db?download=1"
+    expected_md5 <- "f6a4033d376278cccfc30d1911fb2fa5"
+    cache_hit <- BiocFileCache::bfcquery(bfc, cache_name, "rname")
+
+    if (nrow(cache_hit) > 0){
+      rname <- cache_hit$rname[1]
+    }else{
+      rid <- BiocFileCache::bfcadd(bfc, cache_name, cache_url)
+      rname <- BiocFileCache::bfcinfo(bfc, rid = rid)$rname[1]
+    }
+
+    l_dbPth <- BiocFileCache::bfcrpath(bfc, rname)
+    actual_md5 <- tolower(unname(tools::md5sum(l_dbPth)))
+    if (!identical(actual_md5, expected_md5)){
+      stop("Downloaded library database failed MD5 verification.")
+    }
   }
 
   ########################################################
